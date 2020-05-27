b084e1ed84ad11a46994af01301cc18d18e72343
Josafat <josamtz@outlook.com>
2020-05-27 17:57:45 -0500

    🔥 Create Pattern MVP with Swiftlint style

---
+++ firebase-iOS-authentication-example/View/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  firebase-iOS-authentication-example
+//
+//  Created by Josafat Martínez  on 27/05/20.
+//  Copyright © 2020 Josafat Martínez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+}
